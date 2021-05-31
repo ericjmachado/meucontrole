24191e512911c73f28c973fc5f0eb4113f68c597
Eric de jesus machado <ericmachadoeng@gmail.com>
2021-05-30 21:52:59 -0300

    Add host

--- meucontrole/settings.py
+++ meucontrole/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = [
-    '127.0.0.1'
+    '127.0.0.1',
+    'meu-controle.herokuapp.com'
 ]
 
 # Application definition
